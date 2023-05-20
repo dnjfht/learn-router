1e4c09c94b35167e42a6fc19823daa9523b4230f
Yoo Seungmin <dnjfht@gmail.com>
2023-05-20 13:10:04 +0900

    Outlet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav>
-      <Link to="/">Home</Link>
+      <Link to="/" style={{ marginRight: 20 }}>
+        Home
+      </Link>
       <Link to="/videos">Videos</Link>
     </nav>
   );
